01ce3f2919eb5981b9f456662e99575690c676f0
Ilia Kay <ikay2666@gmail.com>
2019-04-16 19:30:04 +0300

    main commit

---
+++ precache-manifest.629f1c7b3236e189c877de5f5dc928f0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f414d9de88710b399f302c6c288760d5",
+    "url": "/static/media/video.f414d9de.mp4"
+  },
+  {
+    "revision": "2740876bbf42b29a256735402e92ca84",
+    "url": "/static/media/img-04.2740876b.jpg"
+  },
+  {
+    "revision": "6ae1c041a3455a17b872430af41772ce",
+    "url": "/static/media/img-03.6ae1c041.jpg"
+  },
+  {
+    "revision": "8aa5b7d0dd6138fc3880f7010481ab16",
+    "url": "/static/media/img-02.8aa5b7d0.jpg"
+  },
+  {
+    "revision": "c2bb827c14eaea0b5d04bd39323e9b1b",
+    "url": "/static/media/img-01.c2bb827c.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "28af19d8990462a5ad60",
+    "url": "/static/js/main.5290d050.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "28af19d8990462a5ad60",
+    "url": "/static/css/main.bc6a8d5e.chunk.css"
+  },
+  {
+    "revision": "413af1a3e2120d092f59402a4107f7db",
+    "url": "/index.html"
+  }
+];
